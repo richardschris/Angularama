2b38c71525cd08468c5587c6ecf3af2445655c2d
Chris Richards <christopher.james.richards@gmail.com>
2015-09-14 20:57:19 -0500

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularamaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularamaApp
+ */
+angular.module('angularamaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularamaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularamaApp
+ */
+angular.module('angularamaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
